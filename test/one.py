64f88d55be3f1d288d695c8e4586ee2a5dde3a13
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-07-13 00:12:37 -0400

    simply directory tree program

--- test/one.py
+++ test/one.py
@@ -1,1 +1,9 @@
-# one
+import os
+def tree_printer(root):
+    for root, dirs, files in os.walk(root):
+        for d in dirs:
+           print(os.path.join(root, d))
+        for f in files:
+            print(os.path.join(root, f))
+
+tree_printer('.')
